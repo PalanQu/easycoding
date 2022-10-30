14de48659a06b36946a5343414feb2ed633eb5f3
Jiabao Qu <palan_work@163.com>
2022-10-30 21:30:02 +0800

    refactor config

--- internal/config/database.go
+++ internal/config/database.go
@@ -1,10 +1,10 @@
 package config
 
 type DatabaseConfig struct {
-	Host           string `mapstructure:"host"`
-	Port           string `mapstructure:"port"`
-	User           string `mapstructure:"user"`
-	Password       string `mapstructure:"password"`
-	DBName         string `mapstructure:"db_name"`
-	CreateDatabase bool   `mapstructure:"create_database"`
+	Host           string `mapstructure:"host" defaultvalue:"localhost"`
+	Port           string `mapstructure:"port" defaultvalue:"3306"`
+	User           string `mapstructure:"user" defaultvalue:"root"`
+	Password       string `mapstructure:"password" defaultvalue:"123456"`
+	DBName         string `mapstructure:"db_name" defaultvalue:"test"`
+	CreateDatabase bool   `mapstructure:"create_database" defaultvalue:"true"`
 }
