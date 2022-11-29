db7f68f6a8fcf01c4a0268737ee6a153883d70f2
Jiabao Qu <palan_work@163.com>
2022-11-29 17:25:48 +0800

    add hooks in ent

--- internal/service/pet/delete_pet.go
+++ internal/service/pet/delete_pet.go
@@ -3,12 +3,15 @@ package pet
 import (
 	"context"
 	pet_pb "easycoding/api/pet"
+	"easycoding/pkg/errors"
 )
 
 func (s *service) deletePet(
 	ctx context.Context,
 	req *pet_pb.DeletePetRequest,
 ) (*pet_pb.DeletePetResponse, error) {
-	s.DB.Pet.DeleteOneID(int(req.PetId))
+	if err := s.DB.Pet.DeleteOneID(int(req.PetId)).Exec(ctx); err != nil {
+		return nil, errors.ErrInvalid(err)
+	}
 	return &pet_pb.DeletePetResponse{}, nil
 }
